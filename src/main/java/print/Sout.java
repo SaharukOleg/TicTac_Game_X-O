e54e1e320e783801034b73ff4856ee34118d6727
Oleg Sakharuk <saharuk_totti10@bk.ru>
2018-10-01 16:53:48 +0300

    Вивід на екран

--- src/main/java/print/Sout.java
+++ src/main/java/print/Sout.java
@@ -4,15 +4,12 @@ import static logic.Main_logic_of_game.ROW;
 import static logic.Main_logic_of_game.playField;
 
 public class Sout {
-
-
     public static void soutFieldOfTheGame() {
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COLUMN; j++) {
                 System.out.print(playField[i][j]);
                 if (j != COLUMN - 1) {
                     System.out.print("|");
-
                 }
             }
 
@@ -21,7 +18,6 @@ public class Sout {
                 System.out.println("-----------");
             }
         }
-
         System.out.println();
     }  // виводить поле гри ОНОВЛЕНЕ
 }
